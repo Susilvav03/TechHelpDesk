3fd0c13eb32db763fae9499f2b0f753dec3b343e
Susilvav03 <Susilvav@unal.edu.co>
2025-12-09 08:55:31 -0500

    feat(technicians): added controllers

--- src/modules/technicians/technicians.controller.ts
+++ src/modules/technicians/technicians.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Roles } from '../../common/decorators/roles.decorator';
+import { TechniciansService } from './technicians.service';
+import { CreateTechnicianDto } from './dto/create-technician.dto';
+import { UpdateTechnicianDto } from './dto/update-technician.dto';
 
+@ApiTags('Technicians')
+@ApiBearerAuth()
+@Roles('ADMIN') // Admin gestiona técnicos
 @Controller('technicians')
-export class TechniciansController {}
+export class TechniciansController {
+  constructor(private readonly techniciansService: TechniciansService) {}
+
+  @Post()
+  create(@Body() dto: CreateTechnicianDto) {
+    return this.techniciansService.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.techniciansService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.techniciansService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateTechnicianDto) {
+    return this.techniciansService.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.techniciansService.remove(id);
+  }
+}
