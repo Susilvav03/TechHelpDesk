a452dbd695da1d6e846739d698286771fdf38eac
juandhm18 <juandhm20@gmail.com>
2025-12-09 07:39:55 -0500

    feat(auth): created jwt strategy and services for the login

---
+++ src/modules/auth/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService) {
+    const jwtSecret = config.get<string>('JWT_SECRET');
+    if (!jwtSecret) {
+      throw new Error('JWT_SECRET environment variable is not defined');
+    }
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtSecret,
+    });
+  }
+
+  async validate(payload: { sub: string; email: string; role: string }) {
+    return { userId: payload.sub, email: payload.email, role: payload.role };
+  }
+}
