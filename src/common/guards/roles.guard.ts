623b8298ca5eabf42b1af6ce3957198b347bc9c0
juandhm18 <juandhm20@gmail.com>
2025-12-09 07:03:24 -0500

    feat(common): added guards

---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,23 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY, Role } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(ctx: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.get<Role[]>(
+      ROLES_KEY,
+      ctx.getHandler(),
+    );
+    if (!requiredRoles || requiredRoles.length === 0) return true;
+
+    const { user } = ctx.switchToHttp().getRequest();
+    return requiredRoles.includes(user?.role);
+  }
+}
