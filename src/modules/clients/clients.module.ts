463cb932a2654eaf18c9fa8103dc871354fe4dea
Susilvav03 <Susilvav@unal.edu.co>
2025-12-09 08:46:22 -0500

    feat(clients): final configuration for  clients module

--- src/modules/clients/clients.module.ts
+++ src/modules/clients/clients.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ClientsController } from './clients.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClientsService } from './clients.service';
+import { ClientsController } from './clients.controller';
+import { Client } from './entities/client.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Client])],
   controllers: [ClientsController],
-  providers: [ClientsService]
+  providers: [ClientsService],
+  exports: [ClientsService, TypeOrmModule],
 })
 export class ClientsModule {}
