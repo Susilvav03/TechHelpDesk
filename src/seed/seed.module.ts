b05cf4a08937809bd6d327160663d739e01e2673
Susilvav03 <Susilvav@unal.edu.co>
2025-12-09 09:29:34 -0500

    feat(seed): Added seed module configuration

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SeedService } from './seed.service';
+import { User } from '../modules/users/entities/user.entity';
+import { Category } from '../modules/categories/entities/category.entity';
+import { Client } from '../modules/clients/entities/client.entity';
+import { Technician } from '../modules/technicians/entities/technician.entity';
 
 @Module({
-  providers: [SeedService]
+  imports: [
+    TypeOrmModule.forFeature([User, Category, Client, Technician]),
+  ],
+  providers: [SeedService],
 })
 export class SeedModule {}
