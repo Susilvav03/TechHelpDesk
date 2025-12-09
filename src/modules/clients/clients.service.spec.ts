b35198e72596a82264c3268abe0cd4f3a1b9298a
Susilvav03 <Susilvav@unal.edu.co>
2025-12-09 08:44:01 -0500

    feat(clients): added the services for the clients CRUD

--- src/modules/clients/clients.service.spec.ts
+++ src/modules/clients/clients.service.spec.ts
@@ -1,18 +1,41 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ClientsService } from './clients.service';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Client } from './entities/client.entity';
+import { CreateClientDto } from './dto/create-client.dto';
+import { UpdateClientDto } from './dto/update-client.dto';
 
-describe('ClientsService', () => {
-  let service: ClientsService;
+@Injectable()
+export class ClientsService {
+  constructor(
+    @InjectRepository(Client)
+    private readonly repo: Repository<Client>,
+  ) {}
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ClientsService],
-    }).compile();
+  async create(dto: CreateClientDto) {
+    const client = this.repo.create(dto);
+    return this.repo.save(client);
+  }
 
-    service = module.get<ClientsService>(ClientsService);
-  });
+  findAll() {
+    return this.repo.find();
+  }
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+  async findOne(id: string) {
+    const client = await this.repo.findOne({ where: { id } });
+    if (!client) throw new NotFoundException('Client not found');
+    return client;
+  }
+
+  async update(id: string, dto: UpdateClientDto) {
+    const client = await this.findOne(id);
+    Object.assign(client, dto);
+    return this.repo.save(client);
+  }
+
+  async remove(id: string) {
+    const client = await this.findOne(id);
+    await this.repo.remove(client);
+    return { deleted: true };
+  }
+}
